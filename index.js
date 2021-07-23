af36ec9cda5a4c7c27b53ec7e715efcd94e35e29
Todd <andrewcunningham770@gmail.com>
2021-07-23 08:31:47 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+let dog =(){
+name ="todd",
+numLeg = 4
+
+}
+
+
+console.log(dog.name)
+console.log(dog.numLeg)
+
+
+let Dog ={
+name:"spot",
+numLegs: 4,
+sayLegs: function() {return "this dog has 4 legs"}
+
+
+}
+  dog.sayLegs();  
+
+
+  let Dog ={
+    name:"spot",
+    numLegs: 4,
+    sayLegs: function() {return "this dog has 4 legs" +
+    this.numLegs + "legs"}
+    
+    }
+      dog.sayLegs();  
+    
+
+function dog {
+ this.name = "rex";
+ this.color = "brown";
+ this.numLegs = 4;
+
+
+
+
+
+}
